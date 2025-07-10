9b2a451d70755794c8bcbce60196208f11592992
Keerthana G Pillai <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-07-10 10:02:28 +0500

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package demo;
+import java.util.Scanner;
+public class Demo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter your age:");
+		int age =sc.nextInt();
+		if (age>18) {
+			System.out.println("Eligible to vote" );
+		}
+		sc.close();
+	}
+}
